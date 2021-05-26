955eb0b2950cb9250ffbe1b47519f9da68b7300c
EverettDeLeon <the00110100lion@gmail.com>
2021-05-26 09:11:57 -0700

    Made the to do list a web app with a server and database using mongoose

--- app.js
+++ app.js
@@ -1,41 +1,133 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const _ = require("lodash");
 
+// boilerplate for express, ejs, bodyParser, file system and mongodb
 const app = express();
-
-var items = ["Eat", "Sleep", "Code"];
-
 app.set("view engine", "ejs");
-
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.use(express.static("public"));
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+
+// create the item data layout
+const itemsSchema = new mongoose.Schema({
+    name: String
+});
+
+// create model for items based on item schema
+const Item = mongoose.model("item", itemsSchema);
+
+// create items within the db
+const eat = new Item({
+    name: "Eat"
+});
+
+const sleep = new Item({
+    name: "Sleep"
+});
+
+const code = new Item({
+    name: "Code"
+});
+
+// create an array with all the items you created
+const defaultItems = [eat, sleep, code];
+
+const listSchema = {
+    name: String,
+    items: [itemsSchema]
+}
+
+const List = mongoose.model("List", listSchema);
+
 
 app.get("/", function (req, res) {
-    var today = new Date();
+    Item.find({}, function(error, foundItems){
+        if(foundItems.length === 0){
+            // insert that array into the db
+            Item.insertMany(defaultItems, function(err){
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Added default array into the db.");
+                }
+            });
+            res.redirect("/");
+        } else {
+            res.render("list", {listTitle: "Today", newListItems: foundItems});
+        }
+    });
+});
 
-    var options = {
-        weekday: 'long',
-        day: 'numeric',
-        month: 'long'
-
-    };
-
-    var day = today.toLocaleDateString('en-US', options);
+app.get("/:customListName", function(req, res){
+    const customListName = _.capitalize(req.params.customListName);
 
 
-    res.render("list", {kindOfDay: day, newListItems: items});
 
 
+    List.findOne({name: customListName}, function(error, foundList){
+        if (!error){
+            if (!foundList){
+                const list = new List({
+                    name: customListName,
+                    items: defaultItems
+                });
+                list.save();
+                res.redirect("/" + customListName);
+            } else {
+                res.render("list", {listTitle: foundList.name, newListItems: foundList.items});
+            }
+        }
+    });
 });
 
 app.post("/", function(req, res){
-    var item = req.body.newItem;
+    const itemName = req.body.newItem;
+    const listName = req.body.list;
 
-    items.push(item);
-    res.redirect("/");
+    const item = new Item({
+        name: itemName
+    });
+    
+    if(listName === "Today") {
+        item.save();
+        res.redirect("/");
+    } else {
+        List.findOne({name: listName}, function (error, foundList){
+            foundList.items.push(item);
+            foundList.save();
+            res.redirect("/" + listName);
+        });
+    }
+
 });
 
+
+app.post("/delete", function(req, res){
+    const checkedItemId = req.body.checkbox;
+    const listName = req.body.listName;
+
+    if (listName === "Today") {
+        Item.findByIdAndRemove(checkedItemId , function(error){
+            if(!error){
+                console.log("checked item removed from list");
+                res.redirect("/");
+            } 
+        });
+    } else {
+        List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, function(error, foundList){
+            if (!error){
+                res.redirect("/" + listName);
+            }
+        });
+    }
+
+
+
+});
+
+
 app.listen(3000, function () {
-    console.log("running");
+    console.log("Listening to port 3000");
 });
